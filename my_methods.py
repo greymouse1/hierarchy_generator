75cfbf91db5ab7b53e1a21fcc625a3a8226f3d0b
greymouse1 <you@example.com>
2024-02-14 20:06:57 +0100

    Trying to call methods from dataset.py Managed to call function loadData

---
+++ my_methods.py
@@ -0,0 +1,8 @@
+# Import the Dataset class from dataset.py
+from dataset import Dataset
+
+# Instantiate the Dataset class
+dataset = Dataset(name='ahrem_small',path='/Users/shark/Desktop/My Documents/uni/Munster/Possible_thesis/Bonn/virtual_folder/pythonProject/tri/ahrem_small',epsilon=0)
+
+dataset.loadData(0)
+
