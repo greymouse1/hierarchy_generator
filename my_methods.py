f4f7563109504e529745a9af2034aa533aa397d9
greymouse1 <you@example.com>
2024-02-14 20:06:57 +0100

    Trying to call methods from dataset.py

---
+++ my_methods.py
@@ -0,0 +1,7 @@
+# Import the Dataset class from dataset.py
+from dataset import Dataset
+
+# Instantiate the Dataset class
+dataset = Dataset(name='ahrem_small',path='/tri/ahrem_small',epsilon=0.1)
+
+dataset.wkbLoaded()
